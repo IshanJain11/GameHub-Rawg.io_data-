d61b2a78ab9b389ed15baaeea4d942438b4121b5
Ishan Jain <ishanj8@gmail.com>
2024-02-15 14:17:39 +0530

    Add emoji

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -14,10 +14,18 @@ export interface Game {
     background_image: string;
     parent_platforms : {platform: Platform} [];
     metacritic: number;
+    rating_top: number;
   }
   
 
 
-const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {params: {genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder, search: gameQuery.searchText}}, [gameQuery]);
+const useGames = (gameQuery: GameQuery) => 
+useData<Game>
+('/games', 
+{params: {genres: gameQuery.genre?.id, 
+  platforms: gameQuery.platform?.id, 
+  ordering: gameQuery.sortOrder, 
+  search: gameQuery.searchText}}, 
+  [gameQuery]);
 
 export default useGames;
